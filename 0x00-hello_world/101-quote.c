d52c433b76977e0cc2db1132c1c17df09d67ed7e
Gibexz <andrewetedu@gmail.com>
2022-12-08 14:51:01 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	int year, month, day;
+
+	year = 2015;
+	month = 10;
+	day = 19;
+
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
