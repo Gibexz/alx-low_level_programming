c9acbf812460e00ec2b6049dee7d86ba27f17093
Gibexz <andrewetedu@gmail.com>
2022-12-08 14:52:43 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,12 +7,6 @@
  */
 int main(void)
 {
-	int year, month, day;
-
-	year = 2015;
-	month = 10;
-	day = 19;
-
 	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
