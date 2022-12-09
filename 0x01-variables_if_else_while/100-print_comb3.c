d31b77a73a4187573244668eec84450e50d4ba1e
Gibexz <andrewetedu@gmail.com>
2022-12-09 14:38:11 -0800

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a ='0'; a <= '8'; a++)
+	{
+		for (b = '1'; b <= '9'; b++)
+		{
+			if (a != b)
+			{
+				putchar(a);
+				putchar(b);
+				if (a == '8' && b == '9')
+				continue;
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	return (0);
+}
