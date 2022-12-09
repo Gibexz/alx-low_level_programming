bcb8c734df2972f468884c162d06bb9ce6a83696
Gibexz <andrewetedu@gmail.com>
2022-12-09 15:53:11 -0800

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, b, c;
+
+	for (a = '0'; a <= '8'; a++)
+	{
+		for (b = '1'; b <= '9'; b++)
+		{	
+			for (c = '2'; c <= '9'; c++)
+			{
+				if (a < b && b < c)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
+					if ((a == '7' && b == '8') && (b == '8' && c == '9'))
+						break;
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
