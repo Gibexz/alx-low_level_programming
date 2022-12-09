dacd5abcfeac5167c09d9abd849816cc86fb1feb
Gibexz <andrewetedu@gmail.com>
2022-12-09 10:59:04 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = '0';
+	char b = 'a';
+
+	while (a <= '9')
+	{
+		putchar(a);
+		a++;
+	}
+
+	while(b <= 'f')
+	{
+		putchar(b);
+		b++;
+	}
+	putchar('\n');
+	return (0);
+}
+
