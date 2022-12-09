715c99a070fb776f86146b1197bf5a88f5a64af8
Gibexz <andrewetedu@gmail.com>
2022-12-09 09:43:13 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = 0;
+
+	while (a >= 0)
+	{
+		printf("%d\n", a);
+		a++;
+	}
+	return (0);
+}
+
