eaac04390e7deb03a4bc3d766c724df738fdb9b1
Gibexz <andrewetedu@gmail.com>
2022-12-13 14:07:29 -0800

    Task 6 code

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - Number to return it Absoulute valur
+ * @n: Int value to be checked
+ * Return: Absolute value
+ */
+int _abs(int a)
+{
+	if (a < 0)
+	{
+		a = -a
+
+		return (a);	
+	}
+	else
+	{
+		return (a);
+	}
+}
