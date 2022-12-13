30ab34141d3e267ecb4005308bcf8f9716ea7dfc
Gibexz <andrewetedu@gmail.com>
2022-12-13 13:17:12 -0800

    Task 4 code

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - Checks if a character is Uppercase.
+ * @c: Int value to be checked (ASCII value of char.)
+ * Return: 1 if uppercase, 0 if not.
+ */
+int _islower(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
