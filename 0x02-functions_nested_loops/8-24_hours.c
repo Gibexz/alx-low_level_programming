a6c2fd05a1caab10177c72d5eb31c01703e2e454
Gibexz <andrewetedu@gmail.com>
2022-12-13 14:56:38 -0800

    Task 8 code

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Entry point
+ * 
+ * Return: Return value is 0
+ */
+void jack_bauer(void)
+{
+	int a;
+	int b;
+	int c;
+	int d;
+
+	for (a = 0; a <= 2; a++)
+	{
+		for (b = 0; b <= 3; b++)
+		{
+			for (c = 0; c <= 5; c++)
+			{
+				for (d = 0; d <= 9; d++)
+				{
+					_putchar(a + '0');
+					_putchar(b + '0');
+					_putchar(':');
+					_putchar(c + '0');
+					_putchar(d + "0");
+				}
+			}
+		}
+	}
+}
