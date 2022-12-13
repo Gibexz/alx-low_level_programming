3ce393dc189ca36dc3617c2b797bed8a5e18751d
Gibexz <andrewetedu@gmail.com>
2022-12-13 14:31:40 -0800

    Task 7 code

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * print_last_digit - Number to return it Absoulute valur
+ * print_last_digit - Number to return it Absoulute value
  * @Num: Int value to be checked
  * Return: Last digit of a number
  */
 int print_last_digit(int Num)
 {
 	int n = Num % 10;
-	
+
 	if (n < 0)
 		n = -n;
 
