090e13056c4a89919e8b78a09f491c0a65de2735
Gibexz <andrewetedu@gmail.com>
2022-12-13 15:13:08 -0800

    Task 8 code

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -21,7 +21,7 @@ void jack_bauer(void)
 				for (d = 0; d <= 9; d++)
 				{
 					if (a == 2 && b == 3 && c == 5 && d == 9)
-						break;
+						continue;
 					_putchar(a + '0');
 					_putchar(b + '0');
 					_putchar(':');
