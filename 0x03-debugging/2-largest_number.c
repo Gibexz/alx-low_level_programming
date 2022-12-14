7624faa445e6b7ee17c5aea98750fdeaa1490536
Gibexz <andrewetedu@gmail.com>
2022-12-14 20:31:16 +0000

    Debugging 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -14,17 +14,13 @@ int largest_number(int a, int b, int c)
 	int largest;
 
 	if (a > b && a > c)
-	{
 		largest = a;
-	}
-	else if (b > a && b > c)
-	{
+
+	if (b > a && b > c)
 		largest = b;
-	}
-	else if (c > a && c > b)
-	{
+
+	if (c > a && c > b)
 		largest = c;
-	}
 
 	return (largest);
 }
