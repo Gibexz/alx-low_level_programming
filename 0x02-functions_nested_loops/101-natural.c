8a6845132b84c3c3d67f176ab83584ef0d66ab4d
Gibexz <andrewetedu@gmail.com>
2022-12-13 17:38:28 -0800

    Task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - sums multiples of 3 and 5.
+ *
+ *Return: 0
+ */
+int main()
+{
+	int num;
+	int sum;
+	int check3;
+	int check5;
+
+	sum = 0;
+
+	for (num = 1; num < 1024; num++)
+	{
+		check3 = num % 3;
+		check5 = num % 5;
+
+		if (check3 == 0 || check5 == 0)
+			sum =+ num;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
