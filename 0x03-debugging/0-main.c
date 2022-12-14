21626a6dedd750a5d3509ea3f52a72991485d382
Gibexz <andrewetedu@gmail.com>
2022-12-14 11:43:24 -0800

    Debugging 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,7 +2,6 @@
 
 /**
  * main - tests function that prints if integer is positive or negative
- *
  * Return: 0
  */
 
@@ -12,6 +11,6 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
-	
+
 	return (0);
 }
