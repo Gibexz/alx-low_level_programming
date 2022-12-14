ff2da54b6bd66e408790eea03ea7b64c12283663
Gibexz <andrewetedu@gmail.com>
2022-12-13 17:57:55 -0800

    Task 14 100 Fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - sums multiples of 3 and 5.
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	int count = 2;
+	long int i = 1, j = 2, k;
+
+	printf("%lu, ", i);
+	while (count <= 50)
+	{
+		if (count == 50)
+		{
+			printf("%lu\n", j);
+		}
+		else 
+		{
+			printf("%lu, ", j);
+		}
+
+		k = j;
+		j += i;
+		i = k;
+		count++;
+	}
+	return (0);
+}
