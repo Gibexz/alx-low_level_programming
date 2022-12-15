5c6a4e831a6c30a47271d72ff8b8e12acc6c9f5f
Gibexz <andrewetedu@gmail.com>
2022-12-15 21:09:52 +0000

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - code to print a FizzBuzz numbering
+ *
+ *
+ * Return: returns a diagonal line based on value of n
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
+	}
+	putchar('\n');
+}
