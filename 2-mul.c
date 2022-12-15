78b7a2d33bd763a33f80c410f4594db70ba83758
Gibexz <andrewetedu@gmail.com>
2022-12-15 10:23:08 +0000

    2-mul.c functions

---
+++ 2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - function that multiplies two integers
+ * @a: first interger
+ * @b: second integer
+ * Return: returns the multi value
+ */
+int mul(int a, int b)
+{
+	int multi = a * b;
+
+	return (multi);
+}
