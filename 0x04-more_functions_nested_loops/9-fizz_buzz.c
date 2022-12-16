9fc3a01ff4aa931782969da310aea15ab6ad58bd
Gibexz <andrewetedu@gmail.com>
2022-12-16 22:41:38 +0000

    fizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	int i = 0;
 
-	for (i = 1; i <= 100; i++)
+	for (i = 1; i <= 99; i++)
 	{
 		if ((i % 3 == 0) && (i % 5 == 0))
 			printf("FizzBuzz ");
@@ -21,6 +21,15 @@ int main(void)
 		else
 			printf("%d ", i);
 	}
+	for (i = 100; i < 101; i++)
+		if ((i % 3 == 0) && (i % 5 == 0))
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
 	putchar('\n');
 	return (0);
 }
