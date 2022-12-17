7d44b2caa83a4020d989e315560f334667472ff9
Gibexz <andrewetedu@gmail.com>
2022-12-17 02:16:01 +0000

    largest prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - code to print the largest prime factor
+ *
+ *
+ * Return: 0 (Always success)
+ */
+int main(void)
+{
+	long long int Num = 612852475143;
+	int count, largest_p_f;
+
+	for (count = 2; count <= Num; count++)
+	{
+		if (Num > 1)
+		{
+			while (Num % count == 0)
+			{
+
+				Num = Num / count;
+
+				largest_p_f = count;
+
+			}
+		}
+	}
+	printf("%d", largest_p_f);
+	printf("\n");
+}
