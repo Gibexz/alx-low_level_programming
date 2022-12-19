6fcbe1dbaa4aab0daa1aea94903c0e2653140e79
Gibexz <andrewetedu@gmail.com>
2022-12-19 18:55:54 +0000

    Task 1 Solution

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - function to swap the know values of two integers
+ * @a: New interger value
+ * @b: A second new integer value
+ *
+ * Return: Nothing i.e Void.
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,4 @@
 void swap_int(int *a, int *b);
 void reset_to_98(int *n);
 int _putchar(char c);
+void swap_int(int *a, int *b);
