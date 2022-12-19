23f18197230e654c74774570091b21c8dd3f238c
Gibexz <andrewetedu@gmail.com>
2022-12-19 22:48:01 +0000

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: passed string pointer
+ *
+ * Return void
+ */
+void puts2(char *str)
+{
+	int len = 0;
+
+	while (*(str + len) != '\0')
+	{
+		if (len % 2 == 0)
+			puts(*(str + i));
+		len++;
+	}
+}
