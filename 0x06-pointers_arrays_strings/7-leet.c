e36cfafb4eed3d4d420c5d1eb96ed69778c09452
Gibexz <andrewetedu@gmail.com>
2022-12-22 14:29:18 +0000

    Task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * leet - function to encode some alphas to nums
+ * @str: pointer to the string
+ *
+ * Return: str
+ *
+ */
+char *leet(char *str)
+{
+	int i = 0;
+
+	if ((str[i] == 65) || (str[i] == 97))
+	{
+		str[i] = 52;
+	}
+	if ((str[i] == 69) || (str[i] == 101))
+	{
+		str[i] = 51;
+	}
+	if ((str[i] == 79) || (str[i] == 111))
+	{
+		str[i] = 48;
+	}
+	if ((str[i] == 84) || (str[i] == 116))
+	{
+		str[i] = 55;
+	}
+	if ((str[i] == 76) || (str[i] == 108))
+	{
+		str[i] = 49;
+	}
+	return (str);
+}
