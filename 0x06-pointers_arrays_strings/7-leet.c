1778053ba51a5b4a30555bbe9ceabbcebb2891e0
Gibexz <andrewetedu@gmail.com>
2022-12-22 14:35:49 +0000

    Task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,27 +9,30 @@
  */
 char *leet(char *str)
 {
-	int i = 0;
+	int i;
 
-	if ((str[i] == 65) || (str[i] == 97))
+	for (i = 47; str[i] < 123; i++)
 	{
-		str[i] = 52;
-	}
-	if ((str[i] == 69) || (str[i] == 101))
-	{
-		str[i] = 51;
-	}
-	if ((str[i] == 79) || (str[i] == 111))
-	{
-		str[i] = 48;
-	}
-	if ((str[i] == 84) || (str[i] == 116))
-	{
-		str[i] = 55;
-	}
-	if ((str[i] == 76) || (str[i] == 108))
-	{
-		str[i] = 49;
+		if ((str[i] == 65) || (str[i] == 97))
+		{
+			str[i] = 52;
+		}
+		if ((str[i] == 69) || (str[i] == 101))
+		{
+			str[i] = 51;
+		}
+		if ((str[i] == 79) || (str[i] == 111))
+		{
+			str[i] = 48;
+		}
+		if ((str[i] == 84) || (str[i] == 116))
+		{
+			str[i] = 55;
+		}
+		if ((str[i] == 76) || (str[i] == 108))
+		{
+			str[i] = 49;
+		}
 	}
 	return (str);
 }
