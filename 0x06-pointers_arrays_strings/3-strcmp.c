9978662af3a7f443fd5ea41c88d9850f86bb86ce
Gibexz <andrewetedu@gmail.com>
2022-12-22 09:39:08 +0000

    Task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,7 +13,7 @@ int _strcmp(char *s1, char *s2)
 	int diff; /* for differnce(in ascii value) */
 		/* when characters compared are not equal */
 
-	for ((i = 0; s1[i] != '\0' || s2[i] != '\0'; i++))
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 	{
 		if (s1[i] != s2[i])
 		{
