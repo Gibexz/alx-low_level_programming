7c5855ec98099eb94db57dc492d166bbba652068
Gibexz <andrewetedu@gmail.com>
2022-12-22 23:03:30 +0000

    Task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -14,11 +14,10 @@ void print_number(int n)
 		n = n * -1;
 	}
 
-	if (n == 0)
-	       _putchar('0');
+	d = n;
 
-	if (n / 10)
-		print_number(n / 10);
+	if (d / 10)
+		print_number(d / 10);
 
-	_putchar(n % 10 + '0');
+	_putchar(d % 10 + '0');
 }
