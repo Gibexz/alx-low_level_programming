ac6e9ab3f09fa82f3dc4f9a65ed54fa41360c396
Gibexz <andrewetedu@gmail.com>
2022-12-22 23:10:34 +0000

    Task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,7 +8,6 @@
  */
 void print_number(int n)
 {
-	int d;
 
 	if (n < 0)
 	{
@@ -16,13 +15,8 @@ void print_number(int n)
 		n = n * -1;
 	}
 
-	if (n == 0)
-		_putchar('0');
+	if (n / 10)
+		print_number(n / 10);
 
-	d = n;
-
-	if (d / 10)
-		print_number(d / 10);
-
-	_putchar(d % 10 + '0');
+	_putchar(n % 10 + '0');
 }
