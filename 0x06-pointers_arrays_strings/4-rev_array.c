36ffe3653fa5fe3c6edf1bbd4b0aade04b58cbdd
Gibexz <andrewetedu@gmail.com>
2022-12-22 10:11:51 +0000

    Task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * reverse_array - function to reverse an array
+ * @a: array in question
+ * @n: no. of array elements
+ *
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int i, Newarray;
+
+	for (i = n - 1; i < n; i--)
+	{
+		Newarray = *(a + i);
+	}
+}
