bed98bdee1fbcd67682f2d7313fb7cc6efcdcaa1
Gibexz <andrewetedu@gmail.com>
2023-01-02 22:46:13 +0000

    Task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int a = 0;
+	int b = 0;
+
+	while (haystack[a])
+	{
+		while (needle[b])
+		{
+			if (haystack[a + b] != needle[b])
+			{
+				break;
+			}
+			b++;
+		}
+		if (needle[b] == '\0')
+		{
+			return (haystack + a);
+		}
+		a++;
+	}
+	return ('\0');
+}
