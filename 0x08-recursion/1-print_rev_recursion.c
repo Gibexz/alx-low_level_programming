56828645a7385ca6e89b1b83de24ec818309b96f
Gibexz <andrewetedu@gmail.com>
2023-01-04 20:46:15 +0000

    Task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,20 +8,10 @@
  */
 void _print_rev_recursion(char *s)
 {
-	int len = 0;
-	
-	while (*s != '\0')
+	if (*s == '\0')
 	{
-		len++;
-	}
-	len--;
-	
-	if (len == 0)
-	{
-		_putchar(s[0]);
-		_putchar('\n');
 		return;
 	}
-	_putchar(s[len]);
-	_print_rev_recursion(s[len - 1]);
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 }
