201c2186e596dd513aa557129c99814ae178d3cf
Gibexz <andrewetedu@gmail.com>
2023-01-06 21:43:49 +0000

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point function
+ * @argc: no. of argurments
+ * @argv: arguments
+ *
+ * Return: printf
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
