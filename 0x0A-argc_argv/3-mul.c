4c786d2735e177afcf37b1b6d9e340347adbaade
Gibexz <andrewetedu@gmail.com>
2023-01-06 22:17:07 +0000

    Task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - entry point function
+ * @argc: no. of argurments
+ * @argv: arguments
+ *
+ * Return: printf
+ */
+int main(int argc, char *argv[])
+{
+	int i, multy = 1;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			multy *= atoi(argv[i]);
+		}
+		printf("%d\n", multy);
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
