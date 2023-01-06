179bb4444b832e6127fa5b20e7af58d870235dd4
Gibexz <andrewetedu@gmail.com>
2023-01-06 13:53:44 +0000

    Static Lib Task 0 and Directory

---
+++ 0x09-static_libraries/Cfiles/2-strlen.c
@@ -0,0 +1,16 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strlen - function to return a string lenght
+ * @s: string to be checked
+ *
+ * Return: String lenght
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(s);
+	return (len);
+}
