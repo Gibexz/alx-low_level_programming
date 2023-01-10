4614f6ed8c3a6fb79dc1ec99b21b4b62aa78392f
Gibexz <andrewetedu@gmail.com>
2023-01-10 16:10:03 +0000

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -22,13 +22,13 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 	{
-		*s1 = ' ';
-	}
-	if (s2 == NULL)
-	{
-		*s2 = ' ';
+		s1 = "";
 	}
 
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
 	newstr = malloc(k * sizeof(char));
 	if (newstr == NULL)
 	{
