d4d7dbedb4ac4959fe7ff30ed2575a93e4ea2e2c
Gibexz <andrewetedu@gmail.com>
2023-01-12 19:18:19 +0000

    Task 0

--- 0-malloc_checked.c
+++ 0-malloc_checked.c
@@ -4,13 +4,14 @@
 
 /**
  * malloc_checked - function that allocates memory using malloc
- * @b - size of byte to allocated
+ * @b: size of byte to allocated
  *
- * Return - Pointer to allocated memory
+ * Return: Pointer to allocated memory or exit with 98 as status
  */
 void *malloc_checked(unsigned int b)
 {
 	int *mal;
+
 	mal = malloc(b);
 	if (mal == NULL)
 	{
