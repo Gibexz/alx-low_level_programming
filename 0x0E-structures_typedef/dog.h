d46c22371b20051da3e84d694b6659f4d504fbdd
Gibexz <andrewetedu@gmail.com>
2023-01-16 21:56:15 +0000

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - details of a dog
+ *
+ * @name: name of the dog
+ * @owner: owner if the dog
+ * @age: the dog's age
+ *
+ * Decription: 
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif /* DOG_H */
