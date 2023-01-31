58dcaf1c798c325e600c33e2f904884bc0f69d70
Gibexz <andrewetedu@gmail.com>
2023-01-31 19:01:37 +0000

    Task10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -35,6 +35,8 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		dnode = dnode->next;
 		temp->next = dnode->next;
 		free(dnode);
+
+		return (1);
 	}
-	return (1);
+	return (-1);
 }
