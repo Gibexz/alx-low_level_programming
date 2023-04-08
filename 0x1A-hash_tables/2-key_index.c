633b05b4ccc5fb26bad43b97ebcc7e12f0b596a2
Gibexz <andrewetedu@gmail.com>
2023-04-08 00:17:06 +0000

    Task 3 solution

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -9,8 +9,8 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int tempKeyIndex = hash_djb2(key);
-	unsigned long int keyIndex = tempKeyIndex % size;
+	if (!key || !size)
+		return (0);
 
-	return (keyIndex);
+	return (hash_djb2(key) % size);
 }
---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,45 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - function that adds an element to the hash table
+ * @ht: is the hash table you want to add
+ * @key: the key
+ * @value: is the value associated with the key
+ *
+ * Return: if successful 1, else 0.
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *mainArray, *newEl;
+
+	if (ht == NULL || key == NULL || (*key) == '\0' || value == NULL)
+		return (0);
+
+	index = key_index((unsigned char *)key, ht->size);
+
+	mainArray = ht->array[index];
+
+	while (mainArray)
+	{
+		if (strcmp(key, mainArray->key) == 0)
+		{
+			free(mainArray->value);
+			mainArray->value = strdup(value);
+			return (1);
+		}
+		mainArray = mainArray->next;
+	}
+
+	newEl = malloc(sizeof(hash_node_t));
+	if(newEl == NULL)
+		return (0);
+
+	newEl->key = strdup(key);
+	newEl->value = strdup(value);
+	newEl->next = mainArray;
+
+	ht->array[index] = newEl;
+	
+	return (1);
+}
